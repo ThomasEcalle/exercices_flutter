e64369cad2bc43afe665cf71807c935a412eecbc
Ecalle Thomas <thomasecalle@hotmail.fr>
2019-07-12 12:00:01 +0200

    Make sixth exercise to understand basic usage of ListView

---
+++ lib/sixth_understanding_lists/exercice.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+/// Create a layout like :
+/// A list of data
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/sixth_understanding_lists/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lessons/sixth_understanding_lists/solution/solution.dart';
+
+void main() => runApp(Solution());
