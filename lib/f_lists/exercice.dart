55ace07258ddce953f8bbe192dd28c6136eaf1e7
Ecalle Thomas <thomasecalle@hotmail.fr>
2019-10-02 22:34:47 +0200

    Fix initial code from f exercise

--- lib/f_lists/exercice.dart
+++ lib/f_lists/exercice.dart
@@ -9,6 +9,15 @@ import 'package:flutter/material.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container();
   }
 }
